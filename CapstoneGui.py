d198277d18c4475dfd88c0ad9d61cbe734c454dc
Airboy98 <Airboy98@users.noreply.github.com>
2018-10-28 18:32:55 -0500

    removing unneeded files

--- CapstoneGui.py
+++ CapstoneGui.py
@@ -1,5 +1,5 @@
 import kivy
-kivy.require('1.10.0') # replace with your current kivy version !
+kivy.require('1.10.1') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.label import Label
@@ -14,40 +14,46 @@ class LoginScreen(GridLayout):
 	def __init__(self, **kwargs):
 		super(LoginScreen, self).__init__(**kwargs)
 		self.cols = 1
-		
+
 		self.newProfile = (Button(text='New Profile +', font_size=14)) #creates button w/ text
 		self.add_widget(self.newProfile)
 		self.newProfile.bind(on_press=self.newProfile_Click)
 
 	def newProfile_Click(self,instance):
 		box = BoxLayout()
-		
+
 		self.popup = Popup(title='What is your name?',title_align='center',auto_dismiss=False, content=box,size_hint=(None, None), size=(400, 100))
-		
-		
+
+
 		self.text = (TextInput(multiline=False, font_size=24))
 		createButton = (Button(text='Create Profile',size_hint=(.5, 1)))
 		cancelButton = (Button(text='Cancel',size_hint=(.3, 1)))
 		box.add_widget(self.text)
 		box.add_widget(createButton)
-		box.add_widget(cancelButton)	
+		box.add_widget(cancelButton)
 		self.text.bind(on_text_validate=self.Create_Click)
 		cancelButton.bind(on_press=self.popup.dismiss)
 		createButton.bind(on_press=self.Create_Click)
 		self.popup.open()
-		
-			
-	
+
+
 	def Create_Click(self,instance):
 		self.cols += 1
 		self.remove_widget(self.newProfile)
-		self.add_widget(Button(text=self.text.text))
+		newbutton = Button(text=self.text.text, id=self.text.text)
+		newbutton.bind(on_press=self.DBscreen)
+		self.add_widget(newbutton)
 		self.add_widget(self.newProfile)
 		self.popup.dismiss()
 
+	def DBscreen(self,instance):
+		self.clear_widgets()
+		dbScreen.run()
 
-		
-		
+
+
+
+
 
 class MyApp(App):
 
@@ -55,8 +61,7 @@ class MyApp(App):
 		self.title = 'Gro-Log'
 		return LoginScreen()
 
-'''
-class containing code to build the dbscreen. remove triple quotes to run 
+
 class DatabaseScreen(App):
 
 	def build(self):
@@ -64,8 +69,7 @@ class DatabaseScreen(App):
 
 
 dbScreen = DatabaseScreen()
-dbScreen.run()
-'''
+#dbScreen.run()
 
 if __name__ == '__main__':
     MyApp().run()
