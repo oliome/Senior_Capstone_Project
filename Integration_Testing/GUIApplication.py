b1b7cd90a1ec4958fb091c3f0c98c049c8b36401
zluckman <zack.luckman@ttu.edu>
2018-11-10 15:42:34 -0600

    Updating and Creating Integrating testing folder
    Commiting new folders and updated backend to work as independant functions, creating their own connections when called

---
+++ Integration_Testing/GUIApplication.py
@@ -0,0 +1,162 @@
+from kivy.app  import App
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+from kivy.properties  import ListProperty, ObjectProperty
+from kivy.uix.listview import ListView
+from kivy.uix.button import Button
+from random import shuffle
+from functools import partial
+
+#added these two .... Adds the functions, then creates the database if it doesnt exist
+from SQLite_test import *
+profile_table_setup
+
+my_list = ['Zack','Ollie','Reyhan','Aaron']
+
+Builder.load_string('''
+
+#: import Button kivy.uix.button.Button
+
+<MyScreenManager>:
+    box2: box2
+    Screen:
+        name: "openscreen"
+        BoxLayout:
+            orientation: "horizontal"
+            Button:
+                font_size: 32
+                background_normal: ''
+                background_color: 1, .3, .4, .85
+                text: "Gro - Log"
+                on_release:
+                    root.current = "screen2"
+
+    Screen:
+        name: "screen2"
+        on_enter: root.update_buttons()
+        BoxLayout:
+            id: box2
+            orientation: "horizontal"
+     
+            Button:
+                
+                text: "Add New Profile"
+                on_release:
+                    root.current = "screen1"
+
+    Screen:
+        name: "screen1"
+        on_leave: root.ids.textinput.text = ''
+        on_enter: root.ids.textinput.focus = True
+        Popup:
+            title: 'What is your name?'
+            title_align: 'center'
+            auto_dismiss: False
+            content: box
+            pos_hint: {'x': .3, 'y': .4}
+            size_hint: (None, None)
+            size: (400, 100)    
+            BoxLayout:
+                id: box
+                TextInput:
+                    id: textinput
+                    focus: True
+                    text: ''
+                    multiline: False
+                    font_size: 24
+                    on_text_validate: root.added_buttons.append(Button(text=root.ids.textinput.text))
+                    on_text_validate: root.current = "screen2"
+                Button: 
+                    id: createButton
+                    text: 'Create Profile'
+                    size_hint: (.5, 1)
+                    on_press: root.added_buttons.append(Button(text=root.ids.textinput.text))
+                    on_press: root.current = "screen2"
+                Button:
+                    id: cancelButton
+                    text: 'Cancel'
+                    size_hint: (.3, 1)
+                    on_press: root.current = "screen2"   
+
+    Screen:
+        name: "screen3"
+        
+        Button:
+            id: bannerbutton
+            font_size: 15
+            size_hint_y: .04
+            text: ""
+            pos_hint: {"center_x": .5, "top": 1}
+        GridLayout:
+            cols: 2
+            size_hint: .7, .2
+            pos_hint: {"center_x": .5, "top": .6}
+            spacing: 50
+            Button:
+                text: "Add Item"
+                pos_hint: {"bottom": .5}
+                on_press:
+                    app.root.transition.direction = "up"
+                    app.root.current = "additem_screen"
+            Button:
+                text: "View Food Inventory"
+                on_press:
+                    app.root.transition.direction = "left"
+                    app.root.current = "inventory_screen"
+        GridLayout:
+            cols: 1
+            size_hint: .2, .1
+            pos_hint: {"center_x": .5}
+            Button:
+                text: "Profile Select"
+                on_press:
+                    root.transition.direction = "right"
+                    root.current = "screen2"
+
+
+''')
+#instance is the last button added
+class MyScreenManager(ScreenManager):
+    added_buttons = ListProperty([])
+    box2 = ObjectProperty(None)
+
+    def __init__(self,*args, **kwargs):
+      super().__init__(*args, **kwargs)
+      for i in my_list:
+          name = i
+          self.make_buttons(name)
+          
+          
+    def make_buttons(self, name):
+        newbutton = Button(text=name)
+        newbutton.bind(on_press = partial(lambda a:self.auth(name)))
+        self.added_buttons.append(newbutton)
+    
+    def update_buttons(self,*args):
+        
+        #self.box2.clear_widgets()
+        shuffle(self.added_buttons)
+
+        for i in self.added_buttons:
+            name = i.text
+            i.bind(on_press= partial(lambda a:self.auth(name)))
+            self.box2.add_widget(i)
+            
+            
+            
+        self.added_buttons[:] = []
+
+    def auth(self,instance):
+        self.transition.direction = "left"
+        self.current = "screen3" 
+        self.ids.bannerbutton.text = "Welcome " + instance + "!"
+        
+
+  
+class MyApp(App):
+
+    def build(self):
+
+        return MyScreenManager()
+
+MyApp().run()
--- Integration_Testing/SQLite_test.py
+++ Integration_Testing/SQLite_test.py
@@ -5,12 +5,13 @@ import sys
 from sqlite3 import Error
  
  
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
+def create_connection():
+    """ create a  connection to the SQLite 
         specified by the db_file
-    :param db_file: database file
+    :param db_file:  file
     :return: Connection object or None
     """
+    db_file= "gro_log.db"
     try:
         conn = sqlite3.connect(db_file)
         return conn
@@ -19,43 +20,47 @@ def create_connection(db_file):
  
     return None
 
-def profile_table_setup(conn):
+def profile_table_setup():
     """
     Query all rows in the tasks table
     :param conn: the Connection object
     :return:
     """
+    conn = create_connection()
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS profiles (name STRING NOT NULL,user_db STRING NOT NULL);")
 
-def create_profile(conn,name):
+def create_profile(name):
     """
     Query all rows in the tasks table
     :param conn: the Connection object
     :return:
     """
+    conn = create_connection()
     cur = conn.cursor()
     cur.execute("INSERT INTO profiles (name,user_db) values (\""+str(name)+"\",\""+str(name.lower())+"_db\");")
     cur=conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS "+str(name.lower())+"_db(barcode INTEGER NOT NULL,item_name STRING NOT NULL, exp DATE);")
-def delete_profile(conn,name):
+def delete_profile(name):
     """
     Query all rows in the tasks table
     :param conn: the Connection object
     :return:
     """
+    conn = create_connection()
     cur = conn.cursor()
     cur.execute("DELETE FROM profiles WHERE name =\""+name+"\";")
     cur.execute("DROP "+ str(name.lower())+"_db;")
 
 
  
-def select_all_profiles(conn):
+def select_all_profiles():
     """
     Query all rows in the tasks table
     :param conn: the Connection object
     :return:
     """
+    conn = create_connection()
     cur = conn.cursor()
     cur.execute("SELECT * FROM profiles;")
  
@@ -64,13 +69,14 @@ def select_all_profiles(conn):
     for row in rows:
         print(row[0]+", "+row[1])
  
-def select_inventory(conn, name):
+def select_inventory(name):
     """
     Query tasks by priority
     :param conn: the Connection object
     :param name: name of profile wanted
     :return:
     """
+    conn = create_connection()
     cur = conn.cursor()
     cur.execute("SELECT * FROM "+str(name.lower())+"_db;")
  
@@ -79,13 +85,14 @@ def select_inventory(conn, name):
     for row in rows:
         print(row[0])
 
-def add_inventory(conn, name):
+def add_inventory(name):
     """
     Query tasks by priority
     :param conn: the Connection object
     :param name: name of profile wanted
     :return:
     """
+    conn = create_connection()
     cur = conn.cursor()
     cur.execute("INSERT INTO "+str(name.lower())+" (name,user_db) values (\""+str(name)+"\",\""+str(name.lower())+"_db\");")
  
@@ -94,13 +101,14 @@ def add_inventory(conn, name):
     for row in rows:
         print(row[0])
 
-def select_profile_db(conn, name):
+def select_profile_db(name):
     """
     Query tasks by priority
     :param conn: the Connection object
     :param name: name of profile wanted
     :return:
     """
+    conn = create_connection()
     cur = conn.cursor()
     cur.execute("SELECT name FROM profiles WHERE name=\""+name+"\";")
  
@@ -113,12 +121,8 @@ def select_profile_db(conn, name):
  
  
 def main():
-    database = "gro_log.db"
- 
-    # create a database connection
-    conn = create_connection(database)
-    with conn:
-        profile_table_setup(conn)
+
+         profile_table_setup()
         #--------Add Profile Test------------
         #add = raw_input("Name you would like to add:")
         #create_profile(conn,add)
