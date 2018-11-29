73dd4f70daa717fe448c2e13581151320d82fa7d
Ollie <35780647+OlisaOmekam@users.noreply.github.com>
2018-11-29 11:51:22 -0600

    Add files via upload

--- GroceryLoggerApp.py
+++ GroceryLoggerApp.py
@@ -1,70 +1,105 @@
-from kivy.app import App
+from kivy.app  import App
 from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
-from kivy.properties import ListProperty, ObjectProperty
+from kivy.properties  import ListProperty, ObjectProperty
 from kivy.uix.listview import ListItemButton, ListItemLabel, CompositeListItem, ListView
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 from random import shuffle
 from functools import partial
-
+from SQLite_test import *
 import json
 import requests
 
-my_list = ['Zack', 'Ollie', 'Rehan', 'Aaron']
+profile_table_setup()
+my_list = select_all_profiles()
 item_list = ObjectProperty()
-
+global current_user
+current_user="Zack"
 
 class ItemListButton(ListItemButton):
     pass
 
+class MessageBox(Popup):
+    def __init__(self, obj, **kwargs):
+        super(MessageBox, self).__init__(**kwargs)
+        self.obj = obj
 
-class Other(Popup):
-    pass
-
-
-# instance is the last button added
+#instance is the last button added
 class MyScreenManager(ScreenManager):
     added_buttons = ListProperty([])
     item_list = ObjectProperty()
     box2 = ObjectProperty(None)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for i in my_list:
-            name = i
-            self.make_buttons(name)
-
+    def __init__(self,*args, **kwargs):
+      super().__init__(*args, **kwargs)
+      for i in my_list:
+          name = i
+          self.make_buttons(name)
+          
+          
     def make_buttons(self, name):
-        newbutton = Button(text=name)
-        newbutton.bind(on_press=partial(lambda a: self.auth(name)))
+        newbutton = Button(text=name, id = name)
+        newbutton.bind(on_press = partial(lambda a:self.auth(name)))
         self.added_buttons.append(newbutton)
 
-    def update_buttons(self, *args):
+    def show_inventory(self):
+        print(current_user)
+        return select_inventory(current_user)
 
-        # self.box2.clear_widgets()
-        # shuffle(self.added_buttons) why was this being shuffled in the first place?
 
+    def delete_buttons(self):
+        popup = MessageBox(self)    # pass screen1 object
+        popup.open()
+        global current_user
+        for i in self.box2.children:
+            if i.text==current_user:
+                self.box2.remove_widget(i)
+        delete_profile(current_user)
+        popup.dismiss()
+
+    def create(self,*args):
+        self.current = "create_screen"
+
+    def update_buttons(self):
+
+        #self.box2.clear_widgets()
+        #shuffle(self.added_buttons)
+        add_user = Button(text="Add New Profile")
+        add_user.bind(on_press = self.create)
+        #self.box2.add_widget(add_user)
         for i in self.added_buttons:
             name = i.text
-            i.bind(on_press=partial(lambda a: self.auth(name)))
-            self.box2.add_widget(i)
+            print(i.text)
+            if (name not in select_all_profiles()):
+                create_profile(name)
+        #for j in select_all_profiles():
+         #   newbutton = Button(text=j, id = j)
+          #  newbutton.bind(on_press = partial(lambda a:self.auth(j)))
+           # self.box2.add_widget(newbutton)
+            #self.added_buttons.append(newbutton)
+        for j in self.added_buttons:
+            name = j.text
+            j.bind(on_press= partial(lambda a:self.auth(name)))
+            self.box2.add_widget(j)
+            
+            
+            
+        self.added_buttons[:]=[]
 
-        self.added_buttons[:] = []
-
-    def open_popup(self):
-        popup = Other()
-        popup.open()
-
-    def auth(self, instance):
+    def auth(self,instance):
         self.transition.direction = "left"
         self.current = "menu_screen"
+        global current_user
+        current_user = instance
         self.ids.bannerbutton.text = "Welcome " + instance + "!"
+        self.ids.experationbutton.text = "You have 3 "+ "items expiring soon!"
+
 
     def search_item(self, barcode_number):
         if barcode_number.text != '':
-            r = requests.get(
-                r'https://api.barcodelookup.com/v2/products?barcode=' + barcode_number.text + '&formatted=y&key=vwsib6fj958n3v9vtjt7sgtch7xclr')
+            r = requests.get(r'https://api.barcodelookup.com/v2/products?barcode='+barcode_number.text+'&formatted=y&key=vwsib6fj958n3v9vtjt7sgtch7xclr')
             data = r.json()
             # displaying in json format
             item = data['products'][0]['product_name']
@@ -79,34 +114,34 @@ class MyScreenManager(ScreenManager):
         if month.text == "Select Month" or day.text == "Select Day" or year.text == "Select Year":
             print("Invalid Expiration Date")
         else:
-            date = month.text + "/" + day.text + "/" + year.text
+            date = month.text+"/"+day.text+"/"+year.text
             self.ids.expdate.text = date
             print(date)
 
     def search_recipes(self):
-        # if a list item is selected
+        #if a list item is selected
         if self.item_list.adapter.selection:
             selection = self.item_list.adapter.selection[0].text
             App_ID = 'cf938db6'
             APP_KEY = '91a43a29d2211953084fcca6b71b005b'
-            r = requests.get(
-                'https://api.edamam.com/search?q=' + selection + '&app_id=' + App_ID + '&app_key=' + APP_KEY)
+            r = requests.get('https://api.edamam.com/search?q='+selection +'&app_id='+App_ID+'&app_key='+APP_KEY)
             data = r.json()
             for i in data['hits']:
                 print('*****************************')
                 print('*****************************')
                 data1 = i['recipe']
                 dishName = data1['label']
-                print('Recipe for ' + dishName)
+                print('Recipe for '+dishName)
+                # prints ingredients needs to print recipe, popup content box layout including label and lable = data, right arror index + 1, left arrow index -1 if index = 0, left arrow == last element of array vise versa..!! change ingredients line
                 for recipe in data1['ingredientLines']:
-                    print(recipe)
-
-
+                    print(recipe)  
+     
+#On submit, pass the data in the spinners for the experation dates, easily done imo, errors could arise in passing from class to class
+  
 class GroceryLoggerApp(App):
 
     def build(self):
         self.title = "Gro-Log"
         return MyScreenManager()
-
 
 GroceryLoggerApp().run()
---
+++ SQLite_test.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python
+ 
+import sqlite3
+import sys
+from sqlite3 import Error
+ 
+ 
+def create_connection():
+    """ create a  connection to the SQLite 
+        specified by the db_file
+    :param db_file:  file
+    :return: Connection object or None
+    """
+    db_file= "gro_log.db"
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None
+
+def profile_table_setup():
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    conn = create_connection()
+    with conn:
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS profiles (name STRING NOT NULL,user_db STRING NOT NULL)")
+
+def create_profile(name):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    conn = create_connection()
+    with conn:
+        cur = conn.cursor()
+        cur.execute("INSERT or IGNORE INTO profiles (name,user_db) values (\""+str(name)+"\",\""+str(name.lower())+"_db\")")
+        cur=conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS "+str(name.lower())+"_db(barcode INTEGER NOT NULL,item_name STRING NOT NULL, exp DATE)")
+
+def delete_profile(name):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    conn = create_connection()
+    with conn:
+        cur = conn.cursor()
+        cur.execute("DELETE FROM profiles WHERE name =\""+name+"\"")
+        cur=conn.cursor()
+        cur.execute("DROP TABLE "+ str(name.lower())+"_db")
+
+
+ 
+def select_all_profiles():
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    conn = create_connection()
+    with conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM profiles")
+    
+        rows = cur.fetchall()
+        array=[]
+        for row in rows:
+            array.append(row[0])
+        return array
+ 
+def select_inventory(name):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param name: name of profile wanted
+    :return:
+    """
+    conn = create_connection()
+    with conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM "+str(name.lower())+"_db")
+    
+        rows = cur.fetchall()
+        array=[]
+        max_len=0
+        for row in rows:
+                if len(row[1]) > max_len:
+                        max_len=len(row[1])
+        for row in rows:
+                temp=str(row[1])
+                x=0
+                pad=" "
+                length= len(temp)
+                if length < max_len:
+                        temp=(pad*(max_len-length)*2)+" "+temp
+
+                while x <= (58-length):
+                        temp= temp+pad
+                        x+=1
+                temp=temp+str(row[2])+pad*48+str(row[0])
+                if length < max_len:
+                        temp=temp+(pad*(max_len-length))
+                array.append(temp)
+        return array
+
+def add_inventory(name,barcode,item_name,exp_date):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param name: name of profile wanted
+    :return:
+    """
+    conn = create_connection()
+    with conn:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO "+str(name.lower())+"_db (barcode,item_name,exp) values ("+barcode+",\""+item_name+"\",\""+exp_date+"\")")
+
+def select_profile_db(name):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param name: name of profile wanted
+    :return:
+    """
+    conn = create_connection()
+    with conn:
+        cur = conn.cursor()
+        cur.execute("SELECT name FROM profiles WHERE name=\""+name+"\"")
+ 
+        rows = cur.fetchall()
+        array=[]
+        for row in rows:
+            array.append(row[0])
+        print(array)
+        return array
+
+ 
+ 
+#def main():
+
+        #profile_table_setup()
+        #--------Add Profile Test------------
+        #add = input("Name you would like to add:")
+        #create_profile(add)
+
+        #print(select_all_profiles())
+
+        #-------Delete Profile Test----------
+        #delete= input("Name you would like to delete:")
+        #delete_profile(conn,delete)
+
+        #-------Select Profile Test----------
+        #name= raw_input("Name you would like to select:")
+        #select_profile_db(conn,name)
+ 
+        #-------Show All Profiles Test-------
+        #print("\n2. Query all profiles")
+        #select_all_profiles(conn)
+
+        #-------Add Item Test---------
+        #name=input("Database name: ")
+        #barcode=input("Barcode: ")
+        #item_name=input("Item name: ")
+        #exp_date=input("Exp Date(YYYY-MM-DD): ")
+        #add_inventory(name,barcode,item_name,exp_date)
+
+        #select_inventory("Zack")
+ 
+ 
+#if __name__ == '__main__':
+#   main()
